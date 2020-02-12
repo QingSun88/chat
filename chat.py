a7a5c6daf800c9e0b037d3803c457ffced859cb3
Qing Sun <sunqingshine@163.com>
2020-02-12 15:10:46 +0100

    sencond

--- chat.py
+++ chat.py
@@ -1,5 +1,5 @@
 
-
+#读取档案
 def read_file(filename):
 	lines = []
 	with open(filename, 'r', encoding='utf-8') as f:
@@ -7,9 +7,10 @@ def read_file(filename):
 			lines.append(line.strip())
 	return lines
 
+#转换档案
 def convert(lines):
-	new = []
-	person = None
+	new = []    #暂存时候则考虑创建变数
+	person = None #避免开头不是人名出现死循环，就先预设空无的值
 	for line in lines:
 		if line =='sunqing':
 			person = 'sunqing'
@@ -17,10 +18,11 @@ def convert(lines):
 		elif line =='brant':
 			person = 'brant'
 			continue
-		if person:
+		if person:  #如果person有值则是真
 		 	new.append(person + ': ' + line)
 	return new
 
+#输出档案
 def write_file(filename, lines):
 	with open(filename,'w') as f:
 		for line in lines:
